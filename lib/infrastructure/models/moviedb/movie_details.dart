15fc2251d232c305d82f75523fcca04d6277ebfb
Eduard <eduard@gmail.com>
2025-10-07 01:22:18 -0500

    los cambios mas dificles hasta el momento terminado

--- lib/infrastructure/models/moviedb/movie_details.dart
+++ lib/infrastructure/models/moviedb/movie_details.dart
@@ -15,7 +15,7 @@ class MovieDetails {
     final String posterPath;
     final List<ProductionCompany> productionCompanies;
     final List<ProductionCountry> productionCountries;
-    final DateTime releaseDate;
+    final DateTime? releaseDate;
     final int revenue;
     final int runtime;
     final List<SpokenLanguage> spokenLanguages;
@@ -59,29 +59,33 @@ class MovieDetails {
         adult: json["adult"],
         backdropPath: json["backdrop_path"] ?? '',
         belongsToCollection: json["belongs_to_collection"],
-        budget: json["budget"],
+        budget: json["budget"] ?? 0,
         genres: List<Genre>.from(json["genres"].map((x) => Genre.fromJson(x))),
-        homepage: json["homepage"],
+        homepage: json["homepage"] ?? '',
         id: json["id"],
-        imdbId: json["imdb_id"],
+        imdbId: json["imdb_id"] ?? '',
         originCountry: List<String>.from(json["origin_country"].map((x) => x)),
-        originalLanguage: json["original_language"],
-        originalTitle: json["original_title"],
-        overview: json["overview"],
-        popularity: json["popularity"]?.toDouble(),
-        posterPath: json["poster_path"],
+        originalLanguage: json["original_language"] ?? '',
+        originalTitle: json["original_title"] ?? '',
+        overview: json["overview"] ?? '',
+        popularity: json["popularity"]?.toDouble() ?? 0,
+        posterPath: json["poster_path"] ?? '',
         productionCompanies: List<ProductionCompany>.from(json["production_companies"].map((x) => ProductionCompany.fromJson(x))),
         productionCountries: List<ProductionCountry>.from(json["production_countries"].map((x) => ProductionCountry.fromJson(x))),
-        releaseDate: DateTime.parse(json["release_date"]),
-        revenue: json["revenue"],
-        runtime: json["runtime"],
+        releaseDate:
+        json["release_date"] != null &&
+            json["release_date"].toString().isNotEmpty
+            ? DateTime.parse(json["release_date"])
+            : null,
+        revenue: json["revenue"] ?? 0,
+        runtime: json["runtime"] ?? 0,
         spokenLanguages: List<SpokenLanguage>.from(json["spoken_languages"].map((x) => SpokenLanguage.fromJson(x))),
-        status: json["status"],
-        tagline: json["tagline"],
-        title: json["title"],
-        video: json["video"],
-        voteAverage: json["vote_average"]?.toDouble(),
-        voteCount: json["vote_count"],
+        status: json["status"] ?? '',
+        tagline: json["tagline"] ?? '',
+        title: json["title"] ?? '',
+        video: json["video"] ?? false,
+        voteAverage: json["vote_average"]?.toDouble() ?? 0,
+        voteCount: json["vote_count"] ?? 0,
     );
 
     Map<String, dynamic> toJson() => {
@@ -101,7 +105,9 @@ class MovieDetails {
         "poster_path": posterPath,
         "production_companies": List<dynamic>.from(productionCompanies.map((x) => x.toJson())),
         "production_countries": List<dynamic>.from(productionCountries.map((x) => x.toJson())),
-        "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
+        "release_date": releaseDate != null
+            ? "${releaseDate!.year.toString().padLeft(4, '0')}-${releaseDate!.month.toString().padLeft(2, '0')}-${releaseDate!.day.toString().padLeft(2, '0')}"
+            : null,
         "revenue": revenue,
         "runtime": runtime,
         "spoken_languages": List<dynamic>.from(spokenLanguages.map((x) => x.toJson())),
--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -138,7 +138,7 @@ class _Slide extends StatelessWidget {
                 Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
                 const SizedBox(width: 3),
                 Text(
-                  HumanFormats.numberDecimal(movie.voteAverage),
+                  HumanFormats.number(movie.voteAverage,1),
                   style: textStyle.bodyMedium?.copyWith(
                     color: Colors.yellow.shade800,
                   ),
