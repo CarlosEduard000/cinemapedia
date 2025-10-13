cfb4aed6f225ed884456c819a5b2763a96298450
Eduard <eduard@gmail.com>
2025-10-13 00:56:10 -0500

    finalizado

--- lib/infrastructure/datasources/actor_moviedb_datasource.dart
+++ lib/infrastructure/datasources/actor_moviedb_datasource.dart
@@ -18,7 +18,7 @@ class ActorMoviedbDatasource extends ActorsDatasource {
   @override
   Future<List<Actor>> getActorsByMovie(String movieId) async {
 
-    final response = await dio.get('/movie/${movieId}/credits');
+    final response = await dio.get('/movie/$movieId/credits');
 
     final castResponse = CreditsResponse.fromJson(response.data);
 
